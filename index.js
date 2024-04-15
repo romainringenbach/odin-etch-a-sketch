46a6dcac872f29fdd50e97bbe9ed89bef2c8e39f
romainringenbach <5659380+romainringenbach@users.noreply.github.com>
2024-04-15 15:35:12 +0200

    Add the drawing logic
    Also, changed a bit the appearance of the grid.

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const grid = document.querySelector(".grid");
 for (let index = 0; index < (16*16); index++) {
     const gridElement = document.createElement("div");
     gridElement.className = "grid-element";
-    grid.appendChild(gridElement);    
+    grid.appendChild(gridElement);
 }
+
+const gridElements = document.querySelectorAll(".grid-element")
+gridElements.forEach((gridElement) => {
+    gridElement.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+  });
